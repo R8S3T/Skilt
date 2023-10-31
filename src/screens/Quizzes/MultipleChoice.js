4029c123c2a602f10dbdb32101f22e0da4559855
Rebecca <r.stelter@gmx.com>
2023-10-31 20:30:11 +0100

    Refactor: Extract multiple choice logic to its own component.

--- src/screens/Quizzes/MultipleChoice.js
+++ src/screens/Quizzes/MultipleChoice.js
@@ -1,32 +1,98 @@
-import React, { useState } from 'react';
-import { View, Text, Button } from 'react-native';
+// QuizTypes/MultipleChoice.js
 
-const MultipleChoice = ({ question, answers, correctAnswer, onAnswer }) => {
-    const [selected, setSelected] = useState(null);
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
 
-    const handleAnswer = (answer) => {
-        setSelected(answer);
-        const isCorrect = answer === correctAnswer;
-        onAnswer(isCorrect)
-    };
+const MultipleChoice = ({ quiz, onContinue }) => {
+    const [selectedOption, setSelectedOption] = useState(null);
+    const [isCorrect, setIsCorrect] = useState(null);
 
-    if (!answers || !Array.isArray(answers)) {
-        return <Text>No answers provided</Text>;
+    const handleAnswer = (optionNumber) => {
+        const correctOptionNumber = getOptionNumber(quiz.Answer);
+        const correctAnswerSelected = optionNumber === correctOptionNumber;
+        setSelectedOption(optionNumber);
+        setIsCorrect(correctAnswerSelected);
+    }
+
+    const getButtonStyle = (index) => {
+        if (selectedOption === null) {
+            return styles.button;
+        }
+        if (selectedOption === (index + 1)) {
+            return (selectedOption === getOptionNumber(quiz.Answer)) ? styles.correctButton : styles.wrongButton;
+        }
+        return styles.button;
+    }
+
+    const optionMapping = {
+        'Option1': 1,
+        'Option2': 2,
+        'Option3': 3,
+        'Option4': 4
+    }
+
+    const getOptionNumber = (answerText) => {
+        const optionKey = Object.keys(quiz).find(key => quiz[key] === answerText);
+        return optionMapping[optionKey];
     }
 
     return (
-        <View>
-            <Text>{question}</Text>
-            {answers.map((answer, index) => (
-                <Button
-                    key={index}
-                    title={answer}
-                    onPress={() => handleAnswer(answer)}
-                    color={selected === answer ? 'green' : 'blue'}
-                />
-            ))}
+        <View style={styles.container}>
+            <Text style={styles.quizText}>{quiz.Question}</Text>
+            {['Option1', 'Option2', 'Option3', 'Option4'].map((option, index) => {
+                const buttonStyle = getButtonStyle(index);
+
+                return (
+                    <TouchableOpacity
+                        key={index}
+                        onPress={() => handleAnswer(index +1)}
+                        style={buttonStyle}
+                    >
+                        <Text style={styles.buttonText}>{quiz[option]}</Text>
+                    </TouchableOpacity>
+                );
+            })}
+            {selectedOption !== null && (isCorrect ? <Text>Correct answer</Text> : <Text>Wrong answer, try again</Text>)}
+            {isCorrect && <Button title='Continue' onPress={onContinue} />}
         </View>
     );
-};
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 16,
+        backgroundColor: '#f2f2f2',
+        marginVertical: 8,
+        borderRadius: 4,
+    },
+    quizText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    button: {
+        backgroundColor: '#fff',
+        padding: 10,
+        marginVertical: 5,
+        borderRadius: 4,
+        alignItems: 'center',
+    },
+    correctButton: {
+        backgroundColor: 'green',
+        padding: 10,
+        marginVertical: 5,
+        borderRadius: 4,
+        alignItems: 'center',
+    },
+    wrongButton: {
+        backgroundColor: 'red',
+        padding: 10,
+        marginVertical: 5,
+        borderRadius: 4,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: 'black',
+    }
+});
 
 export default MultipleChoice;
--- src/screens/Quizzes/QuizScreen.js
+++ src/screens/Quizzes/QuizScreen.js
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import useFetchData from "../../utilities/useFetchData";
-import { PanGestureHandler } from "react-native-gesture-handler";
+import MultipleChoice from "./MultipleChoice";
 
-const QuizScreen = ({ contentId, handleQuizResult, onContinue }) => {
+const QuizScreen = ({ contentId, onContinue }) => {
     const query = 'SELECT * FROM Quiz WHERE ContentId =?';
     const { data, error } = useFetchData(query, [contentId]);
-
-    const [selectedOption, setSelectedOption] = useState(null);
-    const [isCorrect, setIsCorrect] = useState(null);
 
     if (error) {
         return <Text>Error loading quiz.</Text>;
@@ -20,121 +17,18 @@ const QuizScreen = ({ contentId, handleQuizResult, onContinue }) => {
 
     const quiz = data[0];
 
-    const getButtonStyle = (index) => {
-        if (selectedOption === null) {
-            return styles.button;
-        }
-        if (selectedOption === (index + 1)) {
-            return (selectedOption === getOptionNumber(quiz.Answer)) ? styles.correctButton : styles.wrongButton;
-        }
-        return styles.button;
-    }
-
-    // Turn answers into integer in order to get the correct answer
-    const optionMapping = {
-        'Option1': 1,
-        'Option2': 2,
-        'Option3': 3,
-        'Option4': 4
-    }
-
-    const getOptionNumber = (answerText) => {
-        const optionKey = Object.keys(quiz).find(key => quiz[key] === answerText);
-        return optionMapping[optionKey];
-    }
-
-    const handleAnswer = (optionNumber) => {
-        const correctOptionNumber = getOptionNumber(quiz.Answer);
-        const correctAnswerSelected = optionNumber === correctOptionNumber;
-        setSelectedOption(optionNumber);
-        setIsCorrect(correctAnswerSelected);
-        handleQuizResult && handleQuizResult(optionNumber === correctOptionNumber);
-    }
-
-    const renderContinueButton = () => {
-        if (isCorrect) {
-            return (
-                <Button title='Continue' onPress={onContinue} />
-            )
-        }
-    }
-
-    const renderFeedbackText = () => {
-        if (selectedOption !== null) {
-            return (isCorrect ? <Text>Correct answer</Text> : <Text>Wrong answer, try again</Text>)
-        }
-    }
-
-    const onSwipe = ({ nativeEvent }) => {
-        // Detect swipe to the right
-        if (nativeEvent.velocityX > 0) {
-            // Check if user got answer right
-            if (isCorrect) {
-                onContinue();
-            }
-        }
-    };
-
     return (
-        <PanGestureHandler onGestureEvent={onSwipe}>
-        <View style={styles.container}>
-            <Text style={styles.quizText}>{quiz.Question}</Text>
-            {['Option1', 'Option2', 'Option3', 'Option4'].map((option, index) => {
-                const buttonStyle = getButtonStyle(index);
-
-                return (
-                    <TouchableOpacity
-                        key={index}
-                        title={quiz[option]}
-                        onPress={() => handleAnswer(index +1)}
-                        style={buttonStyle}
-                    >
-                        <Text style={styles.buttonText}>{quiz[option]}</Text>
-                    </TouchableOpacity>
-                );
-            })}
-            {renderFeedbackText()}
-            {renderContinueButton()}
+        <View style={StyleSheet.container}>
+            <MultipleChoice quiz={quiz} onContinue={onContinue}/>
         </View>
-        </PanGestureHandler>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        padding: 16,
+        flex: 1,
         backgroundColor: '#f2f2f2',
-        marginVertical: 8,
-        borderRadius: 4,
     },
-    quizText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    button: {
-        backgroundColor: '#fff',
-        padding: 10,
-        marginVertical: 5,
-        borderRadius: 4,
-        alignItems: 'center',
-    },
-    correctButton: {
-        backgroundColor: 'green',
-        padding: 10,
-        marginVertical: 5,
-        borderRadius: 4,
-        alignItems: 'center',
-    },
-    wrongButton: {
-        backgroundColor: 'red',
-        padding: 10,
-        marginVertical: 5,
-        borderRadius: 4,
-        alignItems: 'center',
-    },
-    buttonText: {
-        color: 'black',
-    }
 });
 
 export default QuizScreen;
