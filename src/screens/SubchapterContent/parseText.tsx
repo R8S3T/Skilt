24c4b103bdb629ee372a63c5debb9513ced57d39
Rebecca <r.stelter@gmx.com>
2024-02-01 07:00:47 +0100

    Update TextComponent with bulb icons as list markers, fix text alignment and wrapping

--- src/screens/SubchapterContent/parseText.tsx
+++ src/screens/SubchapterContent/parseText.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, TextComponent } from 'react-native';
+import { Text, View, StyleSheet, TextComponent, Image } from 'react-native';
 import { scaleFontSize } from '../../utilities/utils';
 
 interface TextComponentProps {
@@ -9,15 +9,19 @@ interface TextComponentProps {
 // This function parses the text and returns an array of React elements with applied styles
 const parseText = ({ content }: TextComponentProps): JSX.Element[] => {
   return content.split('\n').map((line, index) => {
+
+    // Style for header
     if (line.startsWith('HEADER:')) {
       return <Text key={index} style={styles.header}>{line.replace('HEADER:', '').trim()}</Text>;
+
+    // Style for bullet points
     } else if (line.startsWith('-')) {
       return (
         <View key={index} style={styles.listItem}>
-          <Text>
-            <Text style={styles.bulletPoint}>• </Text>
+          <Image source={require('../../../assets/Images/bulb.png')} style={styles.bulbIcon} />
+          <View style={styles.listItemTextView}>
             <Text style={styles.listItemText}>{line.replace('-', '').trim()}</Text>
-          </Text>
+          </View>
         </View>
       );
     } else {
@@ -35,16 +39,23 @@ const parseText = ({ content }: TextComponentProps): JSX.Element[] => {
       fontFamily: 'OpenSans-Regular',
       marginVertical: 10,
       lineHeight: scaleFontSize(22),
+      paddingHorizontal: 5,
     },
     listItem: {
       flexDirection: 'row',
-      alignItems: 'center',
+      alignItems: 'flex-start', 
       marginLeft: 10,
       marginVertical: 2,
       marginBottom: 10,
     },
-    bulletPoint: {
+    bulbIcon: {
       marginRight: 10,
+      width: 20,
+      height: 20,
+      marginTop: 5,
+    },
+    listItemTextView: {
+      flex: 1, // This ensures text does not go off-screen
     },
     listItemText: {
       fontSize: scaleFontSize(14),
